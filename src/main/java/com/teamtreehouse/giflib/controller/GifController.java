928d6c6956f8614bc681a6c64206682bc8ca33f0
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-11 16:58:52 -0600

    added route for /gif to a simple response page

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -12,4 +12,10 @@ public class GifController {
     public String listGifs() {
         return "list of all the GIFs";
     }
+
+    @RequestMapping("/gif")
+    @ResponseBody
+    public String listGif() {
+        return "this is a single GIF";
+    }
 }
