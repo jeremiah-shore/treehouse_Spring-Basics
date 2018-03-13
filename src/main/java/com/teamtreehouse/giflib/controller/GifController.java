5d9ee8e61ae0e5c9143f52b6db0ccdf61d178e6a
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-13 13:12:51 -0600

    added route for gif details page using manually added Gif object

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,11 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -13,8 +16,10 @@ public class GifController {
     }
 
     @RequestMapping("/gif")
-    @ResponseBody
-    public String listGif() {
-        return "this is a single GIF";
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015, 2, 13),
+                "Chris Ramaciotti", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
