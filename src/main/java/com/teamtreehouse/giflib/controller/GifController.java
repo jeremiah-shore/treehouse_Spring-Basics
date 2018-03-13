22ffb3744dbf73309c0c10c79fa0a9783dff19d1
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-13 11:41:11 -0600

    added basic thymeleaf html template and updated route for "/" to use new template

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class GifController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs() {
-        return "list of all the GIFs";
+        return "home";
     }
 
     @RequestMapping("/gif")
