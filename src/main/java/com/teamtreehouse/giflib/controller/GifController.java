64190557d28f3b78476ad6900b6f48cfb2725ba6
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-13 13:34:10 -0600

    added class to store Gif data that is searchable by Gif name

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,6 +1,8 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,8 @@ import java.time.LocalDate;
 
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
 
     @RequestMapping("/")
     public String listGifs() {
