90657bb03f9f8c5612550e2cbae3f22be866cd00
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-14 22:26:10 -0600

    added route to handle requests related to a single category

--- src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
@@ -1,7 +1,9 @@
 package com.teamtreehouse.giflib.controller;
 
 import com.teamtreehouse.giflib.data.CategoryRepository;
+import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Category;
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,6 +16,8 @@ import java.util.List;
 public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private GifRepository gifRepository;
 
     @RequestMapping("/categories")
     public String listCategories(ModelMap modelMap) {
@@ -26,6 +30,10 @@ public class CategoryController {
     public String category(@PathVariable int id, ModelMap modelMap) {
         Category category = categoryRepository.findById(id);
         modelMap.put("category", category);
+
+        List<Gif> gifs = gifRepository.findByCategoryId(id);
+        modelMap.put("gifs", gifs);
+
         return "category";
     }
 
