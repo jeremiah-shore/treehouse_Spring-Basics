3b006fd1b83ba04e76621719a8a97d4106d96309
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-14 22:24:15 -0600

    added route to handle requests related to a single category

--- src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/CategoryController.java
@@ -25,6 +25,7 @@ public class CategoryController {
     @RequestMapping("/category/{id}")
     public String category(@PathVariable int id, ModelMap modelMap) {
         Category category = categoryRepository.findById(id);
+        modelMap.put("category", category);
         return "category";
     }
 
