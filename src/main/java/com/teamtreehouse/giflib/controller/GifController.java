56ecd555eea3dde65e6b88edf90654626d33e2e1
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-03-14 21:23:49 -0600

    updated model to enable access to a list of all the available gifs, and updated the home template to display all gifs in a master-detail format

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
@@ -16,7 +17,9 @@ public class GifController {
     private GifRepository gifRepository;
 
     @RequestMapping("/")
-    public String listGifs() {
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
--- src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
+++ src/main/java/com/teamtreehouse/giflib/data/GifRepository.java
@@ -25,4 +25,8 @@ public class GifRepository {
         }
         return null;
     }
+
+    public List<Gif> getAllGifs() {
+        return ALL_GIFS;
+    }
 }
